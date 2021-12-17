5bf1f03be6d2374bcedd66681d7ec96951410cdd
Kynan Stewart Hughes <mail@kynan.net.au>
2021-12-17 16:56:17 +1100

    URL variables

--- docs/js/BubbleSort.js
+++ docs/js/BubbleSort.js
@@ -23,7 +23,15 @@ type Props = {
   finishCounter: {
     ALGORITHMS: Array<Object>,
     COUNT: number
-  }
+  },
+  fps: string,
+  max_seconds_transition_interval : string,
+  cols: string,
+  rows: string,
+  show_working: string,
+  loop: string,
+  reload_interval: string,
+  constant_transition_speed: string
 }
 */
 const BubbleSort /*: function */ = (props /*: Props */) => {
@@ -33,14 +41,16 @@ const BubbleSort /*: function */ = (props /*: Props */) => {
   useEffect(() => {
     // Config
     const bubbleConf = {
-      FPS: 10,
-      MAX_SECONDS_TRANSITION_INTERVAL: 1,
-      COLS: 4,
-      ROWS: 4,
-      SHOW_WORKING: true,
-      LOOP: true,
-      RELOAD_INTERVAL: 2000,
-      CONSTANT_TRANSITION_SPEED: true,
+      FPS: parseInt(props.fps) || 1,
+      MAX_SECONDS_TRANSITION_INTERVAL:
+        parseInt(props.max_seconds_transition_interval) || 1,
+      COLS: parseInt(props.cols) || 4,
+      ROWS: parseInt(props.rows) || 4,
+      SHOW_WORKING: props.show_working === "true" || false,
+      LOOP: props.loop === "true" || false,
+      RELOAD_INTERVAL: parseInt(props.reload_interval) | 2000,
+      CONSTANT_TRANSITION_SPEED:
+        props.constant_transition_speed === "true" || false,
       FINISH_COUNTER: props.finishCounter,
       CONTAINER_ID: props.containerId,
     };
